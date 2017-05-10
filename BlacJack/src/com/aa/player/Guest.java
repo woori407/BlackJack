8278594fc7ca7c7810abe7c541407ab711749d0a
hkedu <hkedu@192.168.3.122>
2017-05-10 13:43:31 +0900

    TEMP

--- BlacJack/src/com/aa/player/Guest.java
+++ BlacJack/src/com/aa/player/Guest.java
@@ -1,12 +1,35 @@
 package com.aa.player;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.aa.card.CardOne;
+
 public class Guest extends Player{
 
+	@Override
 	public int nextAction(){
 		//0:힛트	1:스테이
+
+
 		int choice = -1;
-		
+
+		Scanner scan = new Scanner(System.in);
+		choice = scan.nextInt();
+
+		while(true){
+			System.out.println("Hit을 원하시면 0입력 - Stay를 원하시면 1입력");
+			if(choice == 1){
+				
+				
+				
+				break;
+			}
+			
+
+
+
+		}
 		return choice;
 	}
-	
 }
--- BlacJack/src/com/aa/player/Player.java
+++ BlacJack/src/com/aa/player/Player.java
@@ -7,6 +7,22 @@ import com.aa.card.CardOne;
 public abstract class Player implements InterfacePlayer{
 	protected ArrayList<CardOne> cards;
 	
+	
+	private static final int CARD_POINT = 16;
+	
+	
+	public Player() {
+		cards = new ArrayList<CardOne>();
+		
+		// TODO Auto-generated constructor stub
+	}
+
+	public void receiveCard(){
+		
+	}
+
+
+
 	public ArrayList<CardOne> getCards() {
 		return cards;
 	}
